d6a4e46cee0660f40fb3284afa47db54939d5453
FREDERIC <kannif@ymail.com>
2018-11-06 04:50:34 +0400

    nuit blanche

--- test/imports/ui/body.js
+++ test/imports/ui/body.js
@@ -34,7 +34,14 @@ Template.body.helpers({
       return Ateliers.find({ownerId}).fetch(); 
   },
 
+  desactive() {
 
+  //$ne:true cest nest pas egal a vrai  $ne: cest la negation 
+    //retourne les atelier qui ont checked qui ne sont pas true 
+    // return Ateliers.find( {  checked: { $ne: true } });
+     //renvoir les atelier active
+    return Ateliers.find( {  checked: { $ne: false } });
+  },
   ct() {
 
     // return Ateliers.find( ownerId).count();
@@ -62,13 +69,26 @@ Template.body.helpers({
 
 Template.body.events({
 
+
+     'click .del': function(event) {
+      const target = event.target;
+    Ateliers.remove(this._id);
+  },
+
     'submit .new-atelier'(event) {
   
       // Prevent default browser form submit
       event.preventDefault();
   
       // Get value from form element
+
+      
       const target = event.target;
+
+      const atnomcuisinier= target.nomcuisinier.value;
+      const atprenomcuisinier = target.prenomcuisinier.value;
+      const atspécialitécuisinier = target.spécialitécuisinier.value;
+
       const title= target.title.value;
       const text = target.text.value;
       const date = target.date.value;
@@ -77,13 +97,21 @@ Template.body.events({
       const dispo = target.dispo.value;
       const prix = target.prix.value;
       const image = target.image.value;
-   
+ 
+
+      // console.log(atnomcuisinier);
+      // console.log(atprenomcuisinier);
+      //   console.log(atspécialitécuisinier);
       // Insert a task into the collection
 
       $('#exampleModal').modal('hide')
 
       Ateliers.insert({
         
+
+        atnomcuisinier,
+        atprenomcuisinier,
+        atspécialitécuisinier,
         title,
         text,
         date,
@@ -102,6 +130,12 @@ Template.body.events({
       });
 
       // Clear form
+
+
+       target.nomcuisinier.value= '';
+       target.prenomcuisinier.value= '';
+      target.spécialitécuisinier.value= '';
+
       target.title.value = '';
       target.text.value = '';
       target.date.value = '';
@@ -121,6 +155,7 @@ Template.body.events({
 
       const hidden = document.querySelector('#edit-id');
       idAtelier=hidden.value;
+      const atelier = Ateliers.findOne({_id:hidden.value});
       // Get value from form element
       const target = event.target;
       const Name= target.Name.value;
@@ -129,30 +164,36 @@ Template.body.events({
       const Email = target.Email.value;
       const Place = target.Place.value;
 
+      const Placedispo = atelier.dispo;
+      console.log(atelier.dispo);
+      // console.log(target);
+      // console.log(Name);
 
-
-      console.log(target);
-      console.log(Name);
-
-      console.log(Prenom);
-      console.log(Tel);
+      // console.log(Prenom);
+      // console.log(Tel);
      
-      console.log(Email);
+      // console.log(Email);
       console.log(Place);
-      console.log(idAtelier);
+      // console.log(idAtelier);
       // Insert a task into the collection
-
-      Reserves.insert({
+     
+      if( parseInt(Place) < parseInt(Placedispo)){
+        console.log('grand');
+        alert("Hello! I am an alert box!");
+      }
+// const dispo =  atelier.dispo - Place;
+// console.log(dispo);
+      // Reserves.insert({
         
-        Name,
-        Prenom,
-        Tel,
-        Email,
-        Place,
-        idAtelier,
-        createdAt: new Date(), // current time
+      //   Name,
+      //   Prenom,
+      //   Tel,
+      //   Email,
+      //   Place,
+      //   idAtelier,
+      //   createdAt: new Date(), // current time
   
-      });
+      // });
 
       // Clear form
       target.Name.value = '';
@@ -171,6 +212,12 @@ Template.body.events({
 
         //Récupère valeur dans l'élément form (formulaire)
         const target = event.target;
+
+        const Enomcuisinier = target.Enomcuisinier.value;
+        const Eprenomcuisinier = target.Eprenomcuisinier.value;
+        const Espécialitécuisinier = target.Espécialitécuisinier.value;
+
+       
         const Etitle = target.Etitle.value;
         const Etext = target.Etext.value;
         const Edate = target.Edate.value;
@@ -183,7 +230,11 @@ Template.body.events({
         
         //Mise à jour de la collection
         Ateliers.update(id, {
-          $set:{title: Etitle, text: Etext, horaire: Ehoraire, duree: Eduree, date: Edate, dispo: Edispo, prix: Eprix, image: Eimage}
+          $set:{title: Etitle, text: Etext, horaire: Ehoraire, duree: Eduree, date: Edate, dispo: Edispo, prix: Eprix, image: Eimage,
+            atnomcuisinier:Enomcuisinier,
+            atprenomcuisinier:Eprenomcuisinier,
+            atspécialitécuisinier:Espécialitécuisinier,
+          }
           });
 
           $('#exampleModal2').modal('hide')
@@ -193,9 +244,9 @@ Template.body.events({
 
   //Permet d'effacer element: db
   Template.carte.events({
-    'click .delete': function() {
-    Ateliers.remove(this._id);
-  }
+  //   'click .delete': function() {
+  //   Ateliers.remove(this._id);
+  // }
 
 });
 
@@ -216,6 +267,13 @@ Template.modal2.events({
       const six = document.querySelector('#editDispo');
       const eight = document.querySelector('#editPrix');
       const nime = document.querySelector('#editImage');
+
+      const ten = document.querySelector('#editnomcuisinier');
+      const eleven = document.querySelector('#editprenomcuisinier');
+      const twelve = document.querySelector('#editspécialitécuisinier');
+
+
+
       const id = document.querySelector('#edit-id');
   
       //Insert les valeurs de la collection dans la cellule
@@ -227,6 +285,13 @@ Template.modal2.events({
       six.value = atelier.dispo;
       eight.value = atelier.prix;
       nime.value = atelier.image;
+
+      ten.value = atelier.atnomcuisinier;
+      eleven.value = atelier.atprenomcuisinier;
+      twelve.value = atelier.atspécialitécuisinier;
+
+    
+
       id.value = idAtelier;
 
     }
@@ -254,6 +319,25 @@ Template.modal2.events({
 
     const hidden = document.querySelector('#edit-id');
     hidden.value =this._id;
+
+
+    const atelier = Ateliers.findOne({_id:hidden.value});
+    console.log(atelier.dispo);
+  },
+
+
+  'click .delete': function(event) {
+    // Reserves.remove(this._id);
+    const target = event.target;
+    console.log(target);
+    console.log(this._id);
+
+    const hidden = document.querySelector('#edit-id');
+    hidden.value =this._id;
+
+
+    // const atelier = Ateliers.findOne({_id:hidden.value});
+    // console.log(atelier.dispo);
   },
   
   
